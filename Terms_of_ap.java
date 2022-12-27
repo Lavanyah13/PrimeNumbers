3226e37e6d0b5cec49b17f410715bb95fcf11def
Lavanyah B <104270776+Lavanyah13@users.noreply.github.com>
2022-12-27 20:09:41 +0530

    Create Terms_of_ap.java

---
+++ Terms_of_ap.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Solution{
+  public static void main( String [] args){
+    Scanner s=new Scanner(System.in):
+    int n = s.nextInt();
+    for (int i=1;count<n;i++){
+      int ap = 3*i+n;
+      if (ap%4!=0){
+        System.out.print(ap + " " );
+        count++;
+      }
+    }
+  }
+}
